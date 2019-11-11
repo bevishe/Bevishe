15c97fbeead11e9b96ca17a30501339f95c9f836
BevisHe <hepengme@163.com>
2019-11-11 20:13:39 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/com/cqupt/thread/Demo1.java
@@ -0,0 +1,11 @@
+package com.cqupt.thread;
+
+public class Demo1 {
+    /*
+    * 并发：指两个或者多个事件在同一个事件段内发生（交替执行）
+    * 并行：指两个或者多个事件在同一时刻发生（同时发生）
+    * */
+    public static void main(String[] args) {
+
+    }
+}
