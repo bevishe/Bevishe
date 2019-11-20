d7c3d52c06f6456872559b5674acbeb757fceb3e
Bevishe <hepengme@163.com>
2019-11-20 21:27:47 +0800

    11.20周三 改正了N皇后中的bug， 1.在对遍历棋子的位置时，不应该对每一个棋子都遍历棋盘，而是第一个放入的就遍历棋盘的第一行，第二放入的就遍历第二行，否则会有很多重复的 2.在对棋子位置是否有效进行判断的时候需要在对整个斜边的判断是代码中的有错误，理解的有问题，只是对挨着的斜边位置进行判断，题目是同行，同列，同斜边都不可以放置其他的皇后

--- src/com/leetcode/Demo22.java
+++ src/com/leetcode/Demo22.java
@@ -15,12 +15,19 @@ public class Demo22 {
 
     }
 
+
+    /*
+    * 该题可以使用回溯法来解决，遍历二叉树，对不符合要求的进行剪枝操作，满足条件的节点用集合来接收
+    * */
     public List<String> generateParenthesis(int n){
         // 1.创建一个ArrayList集合对象
         ArrayList<String> arrayList = new ArrayList<>();
 
-        //
+        //2.当树的深度达到了2n停止，或者是左括号小于有括号的数量，每次都从右边添加括号
+        int deep = 0;
+        while(deep<2*n){
 
+        }
 
         return arrayList;
     }
--- src/com/leetcode/NQueens.java
+++ src/com/leetcode/NQueens.java
@@ -4,6 +4,7 @@ import java.util.Queue;
 
 public class NQueens {
 
+    private static final int N = 5;
     static int n = 0;
 
     /*
@@ -11,69 +12,71 @@ public class NQueens {
     * 八皇后问题：
     * 使用回溯法
     *
-    *
     * */
 
     // Queen（t）摆放第t个皇后
-    private static void queen(int t,int[][] bo){
 
-        if(t>8){
+    private static void queen(int t,int[][] bo){
+        if(t==N){
             NQueens.n += 1;
             System.out.println("该摆放是可行的"+NQueens.n);
+            // 将矩阵结果打印出
+            int[] temp = new int[N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+//                    System.out.print(bo[i][j]+"\t");
+                    if(bo[i][j] ==1) temp[i] = j+1;
+                }
+//                System.out.println();
+            }
+            for (int i = 0; i < temp.length; i++) {
+                System.out.print(temp[i]+"\t");
+            }
+        }
+        for (int i = 0; i <N; i++) {
+            if (feasible(t,i,bo)){
+                bo[t][i] = 1;//可以放置则将棋盘位置置为1
+                queen(t+1,bo);
+                bo[t][i] = 0;//将摆放的位置恢复
+            }
 
-            // 将矩阵结果打印出来
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    System.out.print(bo[i][j]+"\t");
-                }
-                System.out.println();
-            }
-            return;
-        }else{
-            for (int i = 0; i <8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if (feasible(i,j,bo)){
-                        bo[i][j] = 1;//可以放置则将棋盘位置置为1
-                        queen(t+1,bo);
-                        bo[i][j] = 0;//将摆放的位置恢复
-                    }
-                }
-            }
         }
     }
 
     // factorial 判断（row,col）位置是否可行
     private static boolean feasible(int row,int col,int[][] bo){
         // 输入的位置不合法
-        if(row>=8||row<0||col>=8||col<0) return false;
+        if(row>=N||row<0||col>=N||col<0) return false;
         // 遍历的位置已经有皇后了
         if(bo[row][col]==1) return false;
         // 判断行和列是否有冲突
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < N; i++) {
             if(bo[i][col] == 1 || bo[row][i] ==1) return false;
         }
         //判断斜边是否有冲突，有四个方向需要判断
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < N; i++) {
             //左上
-            if(((row-1)>=0 &&(col-1)>=0)&&bo[row-1][col-1]==1) return false;
+            if(((row-i)>=0 &&(col-i)>=0)&&bo[row-i][col-i]==1) return false;
             // 右上
-            if(((row-1)>=0&&(col+1<8))&&bo[row-1][col+1]==1) return false;
+            if(((row-i)>=0&&(col+i<N))&&bo[row-i][col+i]==1) return false;
             //左下
-            if(((row+1<8)&&(col-1>=0))&&bo[row+1][col-1]==1) return false;
+            if(((row+i<N)&&(col-i>=0))&&bo[row+i][col-i]==1) return false;
             //右下
-            if((row+1<8)&&(col+1<8)&&bo[row+1][col+1] == 1) return false;
+            if((row+i<N)&&(col+i<N)&&bo[row+i][col+i] == 1) return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
         // 1.用二维数据来定义棋盘，0代表没有棋子，1代表有棋子
-        int[][] board = new int[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 board[i][j] = 0;
             }
         }
-        queen(1,board);
+
+        queen(0,board);
+        System.out.println(N+"皇后共有"+n+"种解法。");
     }
 }
