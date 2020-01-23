293aa879f040acab8e9842a4bf569f20f1afc7d3
BevisHe <hepengme@163.com>
2020-01-23 13:50:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/com/cqupt/recursive/Demo4.java
+++ src/com/cqupt/recursive/Demo4.java
@@ -1,7 +1,27 @@
 package com.cqupt.recursive;
 
-public class Demo4 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
 
+public class Demo4 {
+    // 递归打印多级文件夹
+    /*
+    * 多级目录打印，就是当前目录的嵌套，遍历之前，无从知道到底有多级目录，所以要用到递归实现
+    *
+    * */
+    public static void main(String[] args) {
+        File file = new File("src\\com\\cqupt\\recursive");
+        method(file);
+    }
+
+    // 定义一个方法，参数传递File类型的目录，对该目录进行遍历
+    private static void method(File filedir){
+        File[] fileList = filedir.listFiles();
+        for (File file : fileList) {
+            if(file.isDirectory()){
+                method(file);
+            }else
+                System.out.println(file);
+        }
     }
 }
